9697620e26230064c7338a735fac2d46daa69c5c
Gabriel Low <lowgabriel213@gmail.com>
2024-08-03 17:58:00 +0800

    #52

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
-  return <h1 className="text-3xl">HomePage</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+    </div>
+  );
 }
