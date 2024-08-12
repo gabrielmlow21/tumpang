09de62c0c71d35b9121f44455bccd72b3091cf3a
Gabriel Low <lowgabriel213@gmail.com>
2024-08-13 00:26:48 +0800

    #56

---
+++ app/providers.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ThemeProvider } from "@/components/theme-providers";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
+  );
+}
