1f2c10b8107c4219607a1e2326bda9a1e07a61c8
Gabriel Low <lowgabriel213@gmail.com>
2024-08-13 20:56:01 +0800

    #62

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,5 +1,3 @@
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 const createProfileAction = async (formData: FormData) => {
