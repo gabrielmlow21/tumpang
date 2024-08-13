8161df5b83d1e5a6e5f7a9f64e36611a2a72d40f
Gabriel Low <lowgabriel213@gmail.com>
2024-08-13 20:26:29 +0800

    #59

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { ThemeProvider } from "@/components/theme-providers";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Toaster />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
